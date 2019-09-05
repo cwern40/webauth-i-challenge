1bfe0b9c3f6d76b763d50db7eaca20695c89aa56
cwern40 <chris.wernli@hotmail.com>
2019-09-04 18:40:04 -0600

    I created the login endpoint and added the restricted middleware to check if the username and password is correct

--- server.js
+++ server.js
@@ -24,4 +24,47 @@ server.post('/api/register', (req, res) => {
         })
 })
 
+server.post('/api/login', restricted, (req, res) => {
+    const { username } = req.headers;
+
+    Users.login({ username })
+        .then(user => {
+            res.status(200).json({
+                message: "Logged in"
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "unexpected error"
+            })
+        })
+})
+
+function restricted(req, res, next) {
+    const { username, password } = req.headers;
+
+    if (username && password) {
+        Users.login({ username })
+            .first()
+            .then(user => {
+                if (user && bcrypt.compareSync(password, user.password)) {
+                    next();
+                } else {
+                    res.status(401).json({
+                        message: "invalid credentials"
+                    })
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "unexpected error"
+                })
+            })
+    } else {
+        res.status(400).json({
+            message: "please provide username and password"
+        })
+    }
+}
+
 module.exports = server;
